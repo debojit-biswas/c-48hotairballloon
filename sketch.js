84235061373621482c79aab0bbcd422d6437afb6
debojit-biswas <biswasdebojit99@gmail.com>
2021-10-07 20:59:09 +0530

    my desigend game updated

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var backGround,backGroundImg,balloon,balloonImg
+var backGround,backGroundImg,balloon,balloonImg,topObstaclesGroup,bottomObstaclesGroup
 
 
 
@@ -8,6 +8,12 @@ function preload()
 {
 backGroundImg=loadImage("assets/bg.png")
 balloonImg=loadAnimation("assets/balloon1.png","assets/balloon2.png","assets/balloon3.png")
+topObstacle1Img=loadImage("assets/obsTop1.png")
+topObstacle2Img=loadImage("assets/obsTop2.png")
+
+bottomObstacle1Img=loadImage("assets/obsBottom1.png")
+bottomObstacle2Img=loadImage("assets/obsBottom2.png")
+bottomObstacle3Img=loadImage("assets/obsBottom3.png")
 }
 
 
@@ -27,10 +33,13 @@ balloon.addAnimation("climb",balloonImg)
 balloon.scale=.3
 
 bottomGround=createSprite(200,590,800,20)
+bottomGround.visible=false
 
+topGround=createSprite(200,10,800,20)
+topGround.visible=false
 
-topGround=createSprite(100,200,20,50)
-
+topObstaclesGroup=new Group()
+bottomObstaclesGroup=new Group ()
 }
  
 function draw()
@@ -50,7 +59,80 @@ if(backGround.x<0)
   backGround.x=width/2
 }
 
+spawnTopObstacles()
+
+spawnBottomObstacles()
+
+
+
 drawSprites()
+
+
+}
+function spawnTopObstacles()
+{
+if (frameCount%70===0)
+{
+
+  topObstacle=createSprite(600,Math.round(random(25,100)),10,10)
+topObstacle.velocityX=-3
+topObstacle.scale=0.15
+
+var rand=Math.round(random(1,2))
+
+switch(rand)
+{
+  case 1 : 
+topObstacle.addImage(topObstacle1Img)
+break ; 
+
+case 2 : 
+topObstacle.addImage(topObstacle2Img)
+break ;
+
+default:
+  break ;
+
+}
+
+}
+}
+
+
+
+function spawnBottomObstacles()
+{
+if (frameCount%70===0)
+{
+
+  bottomObstacle=createSprite(600,470,10,10)
+bottomObstacle.velocityX=-3
+bottomObstacle.scale=0.15
+
+var rand=Math.round(random(1,3))
+
+switch(rand)
+{
+  case 1 : 
+bottomObstacle.addImage(bottomObstacle1Img)
+break ; 
+
+case 2 : 
+bottomObstacle.addImage(bottomObstacle2Img)
+break ;
+
+case 3 : 
+bottomObstacle.addImage(bottomObstacle3Img)
+break ;
+
+default:
+  break ;
+
+}
+
+}
+
+
 
 
 }
